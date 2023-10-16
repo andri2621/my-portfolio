47d46d55f647119978c85e2b1e857200c6c512b7
andri2621 <andri.setiawan996@gmail.com>
2023-10-17 00:11:20 +0700

    feat: change get og function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
-import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import * as React from 'react';
 
@@ -7,50 +6,25 @@ import '@/styles/globals.css';
 // Prevent fontawesome from adding its CSS since we did it manually above:
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
+import { getMetaData } from '@/lib/getMetaData';
+
 import { Providers } from '@/app/providers';
-import { siteConfig } from '@/constant/config';
 
 // prevent fontawesome from adding its CSS since we did it manually
 config.autoAddCss = false;
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.title,
-    template: `%s | ${siteConfig.title}`,
-  },
-  description: siteConfig.description,
-  robots: { index: true, follow: true },
-  icons: {
-    icon: '/favicon/favicon.ico',
-    shortcut: '/favicon/favicon-16x16.png',
-    apple: '/favicon/apple-touch-icon.png',
-  },
-  manifest: `/favicon/site.webmanifest`,
-  openGraph: {
-    url: siteConfig.url,
-    title: siteConfig.title,
-    description: siteConfig.description,
-    siteName: siteConfig.title,
-    images: [`${siteConfig.url}/images/og.jpg`],
-    type: 'website',
-    locale: 'en_US',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteConfig.title,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/images/og.jpg`],
-    creator: '@andri2621',
-  },
-  authors: [
-    {
-      name: 'Andi Setiawan',
-      url: 'https://awandri.com',
-    },
-  ],
+const CustomMetaData = {
+  siteName: 'awandri.com',
+  description:
+    'A Showcase of My Project, Blogs, and All About Web Development. ',
+  author: 'Andi Setiawan',
+  userName: 'Frontend Developer',
+  ogType: 'general',
 };
+
+export const metadata = getMetaData(CustomMetaData);
 
 export default function RootLayout({
   children,
